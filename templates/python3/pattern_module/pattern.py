7ccc2056d7ebbf1c3d33c8a2998776b27c2723e8
Erich Gubler <erichdongubler@gmail.com>
2017-01-28 09:19:10 -0700

    Finish Python3 skeleton

---
+++ templates/python3/pattern_module/pattern.py
@@ -0,0 +1,6 @@
+class Pattern(object):
+	def __init__(self):
+		super(Pattern, self).__init__()
+
+	def do_something(self):
+		print("I'm a pattern!")
---
+++ templates/python3/program.py
@@ -0,0 +1,9 @@
+#! /bin/python
+from pattern_module import Pattern
+
+def main():
+	p = Pattern()
+	p.do_something()
+
+if __name__ == "__main__":
+	main()
